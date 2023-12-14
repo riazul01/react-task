bdf0b98901abcf796a17470b70d6ad75883ac4ec
Riazul Islam <riazul5207@gmail.com>
2023-12-14 08:01:00 +0600

    task1 completed

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
     const [item, setItem] = useState({name: '', status: ''});
     const [data, setData] = useState([]);
     const [show, setShow] = useState('all');
 
-    const handleClick = (val) =>{
+    const handleClick = (val) => {
         setShow(val);
     }
 
@@ -13,8 +13,10 @@ const Problem1 = () => {
         setItem({...item, [e.target.name]: e.target.value});
     }
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setData([...data, item]);
+        setItem({name: '', status: ''});
     }
 
     return (
@@ -25,10 +27,10 @@ const Problem1 = () => {
                 <div className="col-6 ">
                     <form onSubmit={handleSubmit} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input value={item.name} onChange={handleChange} type="text" className="form-control" placeholder="Name"/>
+                            <input value={item.name} onChange={handleChange} name="name" type="text" className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input value={item.status} onChange={handleChange} type="text" className="form-control" placeholder="Status"/>
+                            <input value={item.status} onChange={handleChange} name="status" type="text" className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -50,13 +52,18 @@ const Problem1 = () => {
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {data.filter((item) => item.status === show || show === 'all').sort((a, b) => (a.status.toLowerCase() === 'active') ? -1 : (a.status.toLowerCase() === 'completed') ? 1 : 0).map((item, index) => {
+                                return <tr key={index}>
+                                    <td scope="col">{item.name}</td>
+                                    <td scope="col">{item.status}</td>
+                                </tr>
+                            })}
                         </tbody>
                     </table>
                 </div>
